1c1649ca3790212d65bc259cba7d66d2946db5be
RAJEEV SUTRAKAR <sutrakarrajeev22@gmail.com>
2025-11-09 10:30:28 +0530

    added Backend URL

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // change after deployment
+  baseURL: "https://e-commerce-backend-zuxw.onrender.com", // change after deployment
 });
 
 // Add token to headers automatically
